9aa70dc91d7492447b920d502ae88a61c0dbd6a7
kasivisu4 <kasivisu4@gmail.com>
2023-10-15 19:34:10 -0700

    added responsiveness

--- src/App.js
+++ src/App.js
@@ -18,10 +18,128 @@ function App() {
         researchScroll={scrollToResearch}
         activitiesScroll={scrollToActivities}
       ></NavBar>
-      <div className="container d-flex flex-wrap">
-        <div className="right-container">
+      <div className="container d-flex flex-md-nowrap flex-wrap mt-5">
+        <div className="left-container d-flex flex-column position-relative">
+          <div className="text-center">
+            <img
+              src="/profile.jpg"
+              height="300"
+              width="300"
+              className="profile-pic"
+            ></img>
+          </div>
+          <div className="Resume pt-3 flex-md-1">
+            <button className="btn btn-outline-info">
+              <a
+                className="contact-link"
+                href={Resume}
+                download="kasi_resume.pdf"
+              >
+                Here is my resume
+              </a>
+            </button>
+          </div>
+          <div className="contact d-flex flex-md-column flex-wrap  justify-content-center gap-md-0 gap-3">
+            <div className="Mail">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-envelope"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+              </svg>
+              <a
+                className="contact-link p-2"
+                href="mailto:kasivisu3109@gmail.com"
+              >
+                kasivisu3109@gmail.com
+              </a>
+            </div>
+            <div className="Github">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-github"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+              <a
+                className="contact-link p-2"
+                href="https://github.com/kasivisu4"
+              >
+                GitHub
+              </a>
+            </div>
+            <div className="linkedin">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-linkedin"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+              </svg>
+              <a
+                className="contact-link p-2"
+                href="https://www.linkedin.com/in/kasivisu4/"
+              >
+                LinkedIn
+              </a>
+            </div>
+            <div className="Twitter">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-twitter"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+              </svg>
+              <a
+                className="contact-link p-2"
+                href="https://twitter.com/kasi_vandanapu"
+              >
+                Twitter
+              </a>
+            </div>
+            <div className="Facebook">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-facebook"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+              </svg>
+              <a
+                className="contact-link p-2"
+                href="https://www.facebook.com/vandanapu.kasi/"
+              >
+                Facebook
+              </a>
+            </div>
+            {/* <div className="footer">
+              Made in React
+              <img src={logo} className="App-logo" alt="logo" />
+            </div> */}
+          </div>
+        </div>
+
+        <div className="right-container pt-md-0 pt-3">
           <div className="section-1">
-            <div className="Intro">
+            <div className="Intro p-1 p-md-5">
               <p>
                 I am currently pursuing my Master's degree in Computer Science
                 at{" "}
@@ -47,7 +165,7 @@ function App() {
               </a>
             </div>
           </div>
-          <div className="section-2">
+          <div className="section-2 p-1 p-md-5">
             <h5>Work Experience</h5>
             <ul>
               <li>
@@ -97,7 +215,7 @@ function App() {
               </li>
             </ul>
           </div>
-          <div ref={scrollToProjects} className="projects">
+          <div ref={scrollToProjects} className="projects p-1 p-md-5">
             <h5>Projects</h5>
             <ul>
               <li>
@@ -138,7 +256,7 @@ function App() {
               </li>
             </ul>
           </div>
-          <div ref={scrollToPublications} className="publication">
+          <div ref={scrollToPublications} className="publication p-1 p-md-5">
             <h5>Publications / Contributions </h5>
             <ul>
               <li>
@@ -172,7 +290,7 @@ function App() {
               </li>
             </ul>
           </div>
-          <div ref={scrollToResearch} className="research">
+          <div ref={scrollToResearch} className="research p-1 p-md-5">
             <h5>Research</h5>
             <ul>
               <li>
@@ -207,7 +325,7 @@ function App() {
               </li>
             </ul>
           </div>
-          <div ref={scrollToActivities} className="activities">
+          <div ref={scrollToActivities} className="activities p-1 p-md-5">
             <h5> Activities</h5>
             <ul>
               <li>
@@ -226,115 +344,6 @@ function App() {
                 hosted FotoFiesta2k18(flagship event)
               </li>
             </ul>
-          </div>
-        </div>
-        <div className="left-container d-flex flex-row">
-          <img
-            src="/profile.jpg"
-            height="300"
-            width="300"
-            className="img"
-          ></img>
-          <div className="contact">
-            <div className="Resume">
-              <button className="btn btn-outline-info">
-                <a
-                  className="contact-link"
-                  href={Resume}
-                  download="kasi_resume.pdf"
-                >
-                  Here is my resume
-                </a>
-              </button>
-            </div>
-            <div className="Mail">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-envelope"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-              </svg>
-              <a className="contact-link" href="mailto:kasivisu3109@gmail.com">
-                kasivisu3109@gmail.com
-              </a>
-            </div>
-            <div className="Githbu">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-github"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-              </svg>
-              <a className="contact-link" href="https://github.com/kasivisu4">
-                GitHub
-              </a>
-            </div>
-            <div className="linkedin">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-linkedin"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-              </svg>
-              <a
-                className="contact-link"
-                href="https://www.linkedin.com/in/kasivisu4/"
-              >
-                LinkedIn
-              </a>
-            </div>
-            <div className="Twitter">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-twitter"
-                viewBox="0 0 16 16"
-              >
-                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-              </svg>
-              <a
-                className="contact-link"
-                href="https://twitter.com/kasi_vandanapu"
-              >
-                Twitter
-              </a>
-            </div>
-            <div className="Facebook">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-facebook"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-              </svg>
-              <a
-                className="contact-link"
-                href="https://www.facebook.com/vandanapu.kasi/"
-              >
-                Facebook
-              </a>
-            </div>
-            {/* <div className="footer">
-              Made in React
-              <img src={logo} className="App-logo" alt="logo" />
-            </div> */}
           </div>
         </div>
       </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,72 +1,74 @@
-import "./NavBar.css";
+import React, { useState } from "react";
 function NavBar({
   projectScroll,
   publicationsScroll,
   researchScroll,
   activitiesScroll,
 }) {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg w-100 border-bottom">
-        <div className="container justify-content-between">
-          <div>
-            <a className="navbar-brand" href="#">
-              Kasi Viswanath Vandanapu
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-          <div
-            className="collapse navbar-collapse right"
-            id="navbarNavDropdown"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <button
-                  className="nav-link"
-                  onClick={() => projectScroll.current.scrollIntoView()}
-                >
-                  Projects
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link"
-                  onClick={() => publicationsScroll.current.scrollIntoView()}
-                >
-                  Publications
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link"
-                  onClick={() => researchScroll.current.scrollIntoView()}
-                >
-                  Research
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link"
-                  onClick={() => activitiesScroll.current.scrollIntoView()}
-                >
-                  Activities
-                </button>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-md w-100 border-bottom fixed-top p-0 bg-white">
+      <div className="container vw-md-100">
+        <a className="navbar-brand" href="#">
+          Kasi Viswanath Vandanapu
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`${
+            isNavCollapsed ? "collapse" : ""
+          } navbar-collapse justify-content-end`}
+          id="navbarNavDropdown"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <button
+                className="nav-link"
+                onClick={() => projectScroll.current.scrollIntoView()}
+              >
+                Projects
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className="nav-link"
+                onClick={() => publicationsScroll.current.scrollIntoView()}
+              >
+                Publications
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className="nav-link"
+                onClick={() => researchScroll.current.scrollIntoView()}
+              >
+                Research
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className="nav-link"
+                onClick={() => activitiesScroll.current.scrollIntoView()}
+              >
+                Activities
+              </button>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
